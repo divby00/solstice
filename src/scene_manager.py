b5d78941b47fe0b1aab34b10e682d615191b7f09
divby00 <divisionby0x00@gmail.com>
2016-12-14 07:38:23 +0100

    Work on transitions

--- src/scene_manager.py
+++ src/scene_manager.py
@@ -1,12 +1,16 @@
 import pygame
 import config
+import transition_manager
 
 
 class SceneManager(object):
     def __init__(self, context, start_scene):
+        self._temp_scene = None
+        self._temp_scene_data = None
         self.screen = context.scr
         self.scenes = context.scenes
         self.cfg = context.cfg
+        self._transition_manager = transition_manager.TransitionManager(context.resourcemanager)
 
         for scene in self.scenes:
             self.scenes[scene].scenemanager = self
@@ -16,18 +20,24 @@ class SceneManager(object):
                 self.current_scene = self.scene
                 self.set(start_scene)
 
+    @property
+    def transition_manager(self):
+        return self._transition_manager
+
     def set(self, scene_name, scene_data=None):
         self.name = scene_name
         self.current_scene.on_quit()
-        self.scene = self.scenes[scene_name]
+        self.scene = self.scenes[self.name]
         self.current_scene = self.scene
         self.fps = self.scene.scene_speed
         # Horrible injection of custom data to scenes after their constructors have been called.
         # I will burn in hell for this.
-        self.scenes[scene_name].scene_data = scene_data
+        self.scenes[self.name].scene_data = scene_data
         self.clock = pygame.time.Clock()
         self.scene.running = True
         self.scene.on_start()
+        self._transition_manager.set('squares_in')
+        self._transition_manager.start()
 
     def run(self):
         while self.scene.running:
@@ -43,7 +53,9 @@ class SceneManager(object):
                     self._update_window_size_in_config(event.size)
 
             self.scene.run()
+            self._transition_manager.run()
             self.scene.render(self.screen)
+            self._transition_manager.render(self.screen)
             pygame.transform.scale(self.screen.virt,
                                    self.screen.scaling_resolution,
                                    self.screen.scaled_virt)
--- src/transition_manager.py
+++ src/transition_manager.py
@@ -1,7 +1,130 @@
 class TransitionManager(object):
 
-    def __init__(self):
-        self._transitions = []
+    def __init__(self, resource_manager):
+        self._resource_manager = resource_manager
+        self._transitions = self._init_transitions()
+        self._transition = None
 
-    def set(self, transition):
-        pass
+    def _init_transitions(self):
+        transition_factory = TransitionFactory(self._resource_manager)
+        transition_list = ['squares_in']
+        return {transition: transition_factory.get(transition) for transition in transition_list}
+
+    def set(self, transition_name):
+        self._transition = self._transitions[transition_name]
+
+    def start(self):
+        self._transition.start()
+
+    def run(self):
+        self._transition.run()
+
+    def render(self, screen):
+        self._transition.render(screen)
+
+    @property
+    def status(self):
+        return self._transition.status
+
+    @status.setter
+    def status(self, value):
+        self._transition.status = value
+
+
+class TransitionFactory(object):
+
+    def __init__(self, resource_manager):
+        self._resource_manager = resource_manager
+
+    def get(self, transition_type):
+
+        if transition_type == 'squares_in':
+            sprites = [self._resource_manager.get('transition' + str(index)) for index in xrange(4, -1, -1)]
+            return TransitionSquaresIn(4, sprites, Transition.START_SCENE)
+
+
+class Transition(object):
+
+    IDLE = 0x01
+    STARTING = 0x02
+    FINISHED = 0x04
+
+    START_SCENE = 1
+    FINISH_SCENE = 2
+
+    def __init__(self, total_frames, sprites, type):
+        self._status = Transition.IDLE
+        self._total_frames = total_frames
+        self._sprites = sprites
+        self._frame = -1
+        self._type = type
+
+    @property
+    def type(self):
+        return self._type
+
+    @property
+    def status(self):
+        return self._status
+
+    @status.setter
+    def status(self, value):
+        self._status = value
+
+    def start(self):
+        self._frame = -1
+        self._status = Transition.STARTING
+
+    def render(self):
+        raise NotImplementedError('Not yet implemented')
+
+    def run(self):
+        if self._status == Transition.IDLE or self._status == Transition.FINISHED:
+            return
+
+        if self._status == Transition.STARTING:
+            if self._frame < self._total_frames:
+                self._frame += 1
+            else:
+                self._status = Transition.FINISHED
+
+
+class TransitionSquaresIn(Transition):
+
+    def __init__(self, total_frames, sprites,type):
+        super(TransitionSquaresIn, self).__init__(total_frames, sprites, type)
+        self._matrix = [
+            [1, 2, 3, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4],
+            [0, 1, 2, 3, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4],
+            [0, 0, 1, 2, 3, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4],
+            [0, 0, 0, 1, 2, 3, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4],
+            [0, 0, 0, 0, 1, 2, 3, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4],
+            [0, 0, 0, 0, 0, 1, 2, 3, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4],
+            [0, 0, 0, 0, 0, 0, 1, 2, 3, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4],
+            [0, 0, 0, 0, 0, 0, 0, 1, 2, 3, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4],
+            [0, 0, 0, 0, 0, 0, 0, 0, 1, 2, 3, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 2, 3, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 2, 3, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 2, 3, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 2, 3, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 2, 3, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 2, 3, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 2, 3, 4, 4, 4, 4, 4, 4, 4, 4, 4],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 2, 3, 4, 4, 4, 4, 4, 4, 4, 4],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 2, 3, 4, 4, 4, 4, 4, 4, 4],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 2, 3, 4, 4, 4, 4, 4, 4],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 2, 3, 4, 4, 4, 4, 4],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 2, 3, 4, 4, 4, 4],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 2, 3, 4, 4, 4],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 2, 3, 4, 4],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 2, 3, 4]
+        ]
+
+    def run(self):
+        super(TransitionSquaresIn, self).run()
+
+    def render(self, screen):
+        if not (self._status == Transition.IDLE or self._status == Transition.FINISHED):
+            for y in xrange(0, 24):
+                for x in xrange(0, 32):
+                    screen.virt.blit(self._sprites[self._frame], (x * 8, y * 8))
